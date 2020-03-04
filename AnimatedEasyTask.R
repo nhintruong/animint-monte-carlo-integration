bd9fb808efe2d59c135e66f7d2421335cab150cb
lazycipher <17141301+lazycipher@users.noreply.github.com>
2020-03-04 06:04:19 +0530

    EasyTask Multi Plot with showSelected & clickSelects

--- AnimatedEasyTask.R
+++ AnimatedEasyTask.R
@@ -1,26 +1,39 @@
 library(animint2)
 UPIData <- read.csv('./data/UPIData.csv')
 
-viz.plot= ggplot()+
-    ggtitle("Comparision of Unified Payments Interface(UPI) Usage Data")+
-    geom_point(aes(x=No.Of.Banks.Using.Upi, y=Volume.Of.Transaction.in.Mn, color=Month, key=Month),
-               size=6,
-               clickSelects="Month",
-               showSelected="Year",
-               data=UPIData)+
-    geom_path(aes(x=No.Of.Banks.Using.Upi, y=Volume.Of.Transaction.in.Mn),
-              alpha=0.3,
-              size=2,
-              data=UPIData)+
-    theme_bw()+
-    theme_animint(width=800, height=500)
+viz.plot.one <- ggplot()+
+  ggtitle("No. of Banks vs. Transaction")+
+  geom_point(aes(x=No.Of.Banks.Using.Upi, y=Volume.Of.Transaction.in.Mn, color=Month, key=Month),
+             size=6,
+             clickSelects="Month",
+             showSelected="Year",
+             data=UPIData)+
+  geom_path(aes(x=No.Of.Banks.Using.Upi, y=Volume.Of.Transaction.in.Mn),
+            size=2,
+            data=UPIData)+
+  theme_bw()
 
-viz.animint <- animint(
-  title="Comparision of Unified Payments Interface(UPI) Usage Data",
+viz.plot.two <- ggplot()+
+  ggtitle("Transaction: Volume vs. Amount")+
+  geom_point(aes(x=Volume.Of.Transaction.in.Mn, y=Amount.Of.Transaction.In.Bn, color=Month, key=Month),
+             size=5,
+             clickSelects="Month",
+             showSelected="Year",
+             data=UPIData)+
+  geom_step(aes(x=Volume.Of.Transaction.in.Mn, y=Amount.Of.Transaction.In.Bn),
+            size=2,
+            linetype=4,
+            data=UPIData)+
+  theme_bw()
+
+(viz.publish <- animint(
+  viz.plot.one,
+  viz.plot.two,
+  title="Unified Payments Interface(UPI) Data India",
   duration=list(Year=1000),
   time=list(variable="Year", ms=2000),
   selector.types=list(Month="multiple"),
-  first=list(Year=2016),
-  viz.plot
-  )
-viz.animint
+  first=list(Year=2016)
+))
+
+animint2gist(viz.publish)
