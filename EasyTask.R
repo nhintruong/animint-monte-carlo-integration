2787f3c9fa8f85a95e89a825acdec0c921795d17
lazycipher <17141301+lazycipher@users.noreply.github.com>
2020-03-08 05:02:27 +0530

    Easy Task Update

--- EasyTask.R
+++ EasyTask.R
@@ -1,34 +1,56 @@
 library(animint2)
-UPIData <- read.csv('./data/UPIData.csv')
 
+data(WorldBank, package="animint2")
+WorldBank <- subset(WorldBank, year>=1980)
+years <- data.frame(year=unique(WorldBank$year))
 
-show.point.list <- list()
-show.path.list <- list()
+viz.one <- ggplot()+
+  ggtitle("Life Expectancy vs Fertility Rate")+
+  geom_point(aes(
+    x=life.expectancy, y=fertility.rate, fill=region,
+    key=country, size=population),
+    showSelected="year",
+    color="#000000"	,
+    clickSelects="country",
+    data=WorldBank)+
+  geom_path(aes(
+    x=life.expectancy, y=fertility.rate, color=region, key=region),
+    showSelected="country",
+    aplha=0.5,
+    data=WorldBank)+
+  geom_text(aes(
+    x=life.expectancy, y=fertility.rate, label=paste(country, "in", year, sep=" "),
+    key=country),
+    showSelected=c("year", "country", "region"),
+    data=WorldBank)+
+  theme_linedraw()+
+  theme_animint(width=1000, height=700)
+  
+viz.two <- ggplot()+
+  ggtitle("Year vs Fertility Rate")+
+  geom_tallrect(aes(
+    xmin=year-0.5, xmax=year+0.5),
+    clickSelects="year",
+    alpha=0.5,
+    data=years)+
+  geom_line(aes(
+    x=year, y=fertility.rate, group=country, color=region),
+    clickSelects="country",
+    size=3,
+    alpha=0.6,
+    data=WorldBank)+
+  geom_text(aes(
+    x=year, y=fertility.rate, label=country, key=country),
+    showSelected=c("year", "country", "region"),
+    data=WorldBank)+
+  theme_linedraw()+
+  theme_animint(width=1000, height=700)
 
-for(show.year in c(2017, 2018, 2019)){
-  show.point.list[[paste(show.year)]] <- data.frame(
-    show.year, subset(UPIData, show.year == Year))
-  show.path.list[[paste(show.year)]] <- data.frame(
-    show.year, subset(UPIData, show.year == Year))
-}
+(viz.animint <- animint(viz.one,
+                        viz.two,
+                        title="World Bank Interactive Visualization",
+                        duration = list(year=2000),
+                        time=list(variable="year", ms=3000)))
 
-show.point <- do.call(rbind, show.point.list)
-show.path <- do.call(rbind, show.path.list)
-
-viz.panels <- list(
-  scatter=ggplot()+
-    ggtitle("Comparision of Unified Payments Interface(UPI) Usage Data")+
-    geom_point(aes(x=No.Of.Banks.Using.Upi, y=Volume.Of.Transaction.in.Mn, color=Month),
-               size=6,
-               data=show.point)+
-    geom_path(aes(x=No.Of.Banks.Using.Upi, y=Volume.Of.Transaction.in.Mn),
-              size=2,
-               data=show.path)+
-    facet_grid(. ~ show.year)+
-    theme_bw()+
-    theme(panel.margin=grid::unit(0, "lines"))+
-    theme_animint(width=800, height=500)
-)
-data_viz <- structure(viz.panels)
-viz.animint <- animint(data_viz$scatter)
-viz.animint
+animint2dir(viz.animint, "output")
+animint2gist(viz.animint, description = "Interactive World Bank Data Viz. 'Life Expectancy vs Fertility Rate and Year vs Fertility Rate'")
---
+++ Other Viz/EasyTask Old.R
@@ -0,0 +1,34 @@
+library(animint2)
+UPIData <- read.csv('./data/UPIData.csv')
+
+
+show.point.list <- list()
+show.path.list <- list()
+
+for(show.year in c(2017, 2018, 2019)){
+  show.point.list[[paste(show.year)]] <- data.frame(
+    show.year, subset(UPIData, show.year == Year))
+  show.path.list[[paste(show.year)]] <- data.frame(
+    show.year, subset(UPIData, show.year == Year))
+}
+
+show.point <- do.call(rbind, show.point.list)
+show.path <- do.call(rbind, show.path.list)
+
+viz.panels <- list(
+  scatter=ggplot()+
+    ggtitle("Comparision of Unified Payments Interface(UPI) Usage Data")+
+    geom_point(aes(x=No.Of.Banks.Using.Upi, y=Volume.Of.Transaction.in.Mn, color=Month),
+               size=6,
+               data=show.point)+
+    geom_path(aes(x=No.Of.Banks.Using.Upi, y=Volume.Of.Transaction.in.Mn),
+              size=2,
+               data=show.path)+
+    facet_grid(. ~ show.year)+
+    theme_bw()+
+    theme(panel.margin=grid::unit(0, "lines"))+
+    theme_animint(width=800, height=500)
+)
+data_viz <- structure(viz.panels)
+viz.animint <- animint(data_viz$scatter)
+viz.animint
